501bae6c7520fa4d4df7ffe24a63e5673934e118
Adham-Magdy <adhammagdy349@gmail.com>
2024-01-23 14:04:55 +0200

    refactor: restructure navBar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,12 +1,18 @@
+'use client';
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
+    // get current path name
+    const currentPathName = usePathname();
+    console.log(currentPathName);
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center justify-between text-xl">
       <Link
@@ -21,7 +27,7 @@ const NavBar = () => {
           <Link
             href={link.href}
             key={link.href}
-            className="text-zinc-400 hover:text-zinc-800 transition-colors"
+            className={`${currentPathName===link.href ? 'text-zinc-800':'text-zinc-400'}  hover:text-zinc-800 transition-colors`}
           >
             {link.label}
           </Link>
