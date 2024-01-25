55447ddd16257f47781a1b480507d3acecdccadd
Adham-Magdy <adhammagdy349@gmail.com>
2024-01-25 17:58:03 +0200

    feat: implementing client side validation

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -2,11 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import IssuesPage from '../../issues/page';
 import { PrismaClient } from "@prisma/client";
+import { createIssueSchema } from "@/app/validationSchemas";
 // using zod to validate data
-const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
-});
+
 export const POST = async (request: NextRequest) => {
   const body = await request.json();
 
@@ -15,7 +13,7 @@ export const POST = async (request: NextRequest) => {
   const validation = createIssueSchema.safeParse(body);
   // check if validation success
   if(!validation.success){
-    return  NextResponse.json(validation.error.errors,{status:400});
+    return  NextResponse.json(validation.error.format(),{status:400});
   }
 
   // sending data to prisma client to insert in db
--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,39 +1,84 @@
-"use client"
-import { Button, TextField, TextFieldRoot } from '@radix-ui/themes'
+"use client";
+import {
+  Button,
+  Callout,
+  CalloutRoot,
+  CalloutText,
+  TextField,
+  Text,
+  TextFieldRoot,
+} from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import React from 'react'
-import { string } from 'zod';
-import { Controller, useForm } from 'react-hook-form';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
+import React, { useState } from "react";
+import { string, z } from "zod";
+import { Controller, useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface IssueForm{
-  title:string;
-  description:string;
-}
+// interface IssueForm {
+//   title: string;
+//   description: string;
+// }
+type IssueForm = z.infer<typeof createIssueSchema>;
 const NewIssuePage = () => {
   // useForm
-  const {register,control,handleSubmit} = useForm<IssueForm>();
+  const { register, control, handleSubmit,formState:{errors}} = useForm<IssueForm>({
+    resolver:zodResolver(createIssueSchema),
+  });
   const router = useRouter();
-  console.log(register('title'));
-  return (
-    <form className = "max-w-xl space-y-3 p-3 border-blue-100 border-2 rounded-md " onSubmit={handleSubmit(async (data)=>{
-      await axios.post("/api/issues",data);
-      router.push('/issues');
-    })}>
-      <TextField.Root>
-        <TextField.Input placeholder='Title' {...register("title")}/>
-      </TextField.Root>
-      {/* Using controller to render markdown editor with useForm  */}
-      <Controller
-      name='description'
-      control={control}
-      render={({field}) => <SimpleMDE  placeholder='Description of issue' {...field}/>}
-      />
-      <Button className='cursor-pointer'>Submit New Issue</Button>
-    </form>
-  )
-}
+  const [error, setError] = useState("");
 
-export default NewIssuePage
+  return (
+    <div className="max-w-xl ">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3 p-3 border-blue-100 border-2 rounded-md "
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("Unexpected error occurred");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        {/* Display title error */}
+        {
+          errors.title && (
+            <Callout.Root color="red" className="mb-5 bg-white">
+          <Callout.Text>{errors.title.message}</Callout.Text>
+        </Callout.Root>
+          )
+        }
+        {/* Using controller to render markdown editor with useForm  */}
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description of issue" {...field} />
+          )}
+        />
+        {
+          errors.description && (
+            <Callout.Root color="red"  className="mb-5 bg-white ">
+          <Callout.Text>{errors.description.message}</Callout.Text>
+        </Callout.Root>
+          )
+        }
+        <Button className="cursor-pointer">Submit New Issue</Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewIssuePage;
