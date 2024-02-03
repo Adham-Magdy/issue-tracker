99ddbe8247e2a23ad9ec2ca82e74a7e0f45ec262
Adham-Magdy <adhammagdy349@gmail.com>
2024-02-03 16:02:59 +0200

    feat: Implement edit issue button

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,45 +1,53 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
-import { PrismaClient } from '@prisma/client';
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
-import delay from 'delay';
-import { notFound } from 'next/navigation';
-import React from 'react'
-import ReactMarkdown from 'react-markdown';
-import { number } from 'zod';
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { PrismaClient } from "@prisma/client";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import delay from "delay";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import { number } from "zod";
 
-interface Props{
-    params:{id: string}; // id in url path is default string
-};
-
-const IssueDetailsPage = async ({params}:Props) => {
-    const prisma = new PrismaClient();
-
-    // validate id number of page
-    // if(typeof params.id !== 'number') notFound();
-    // return issue from db based on id
-    const issue = await prisma.issue.findUnique({
-        where:{id:parseInt(params.id)},
-    });
-
-    // if issue not found
-    if(!issue){
-        notFound();
-    }
-    await delay(2000);
-  return (
-    <div>
-        <Heading>{issue.title}</Heading>
-        <Flex gap={"5"} mb={"5"}>
-            <IssueStatusBadge status={issue.status}/>
-            <Text>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card className='prose' mt={'4'}>
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </Card>
-       
-        
-    </div>
-  )
+interface Props {
+  params: { id: string }; // id in url path is default string
 }
 
-export default IssueDetailsPage
+const IssueDetailsPage = async ({ params }: Props) => {
+  const prisma = new PrismaClient();
+
+  // validate id number of page
+  // if(typeof params.id !== 'number') notFound();
+  // return issue from db based on id
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  // if issue not found
+  if (!issue) {
+    notFound();
+  }
+  await delay(2000);
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap={"5"} mb={"5"}>
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose" mt={"4"}>
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
+  );
+};
+
+export default IssueDetailsPage;
