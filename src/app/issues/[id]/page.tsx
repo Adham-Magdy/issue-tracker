edea569b1f555a0314d0f641ff30811acabe5420
Adham-Magdy <adhammagdy349@gmail.com>
2024-02-03 17:23:41 +0200

    refactor: applying single resbonsibility principle on issue details

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,13 +1,10 @@
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import { PrismaClient } from "@prisma/client";
-import { Pencil2Icon } from "@radix-ui/react-icons";
-import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import { Box,Grid } from "@radix-ui/themes";
 import delay from "delay";
-import Link from "next/link";
 import { notFound } from "next/navigation";
 import React from "react";
-import ReactMarkdown from "react-markdown";
-import { number } from "zod";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
   params: { id: string }; // id in url path is default string
@@ -31,20 +28,10 @@ const IssueDetailsPage = async ({ params }: Props) => {
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Box>
-        <Heading>{issue.title}</Heading>
-        <Flex gap={"5"} mb={"5"}>
-          <IssueStatusBadge status={issue.status} />
-          <Text>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card className="prose" mt={"4"}>
-          <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </Card>
+       <IssueDetails issue={issue}/>
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-        </Button>
+        <EditIssueButton issueID={issue.id} />
       </Box>
     </Grid>
   );
