8a446e45eb0f130ba4beba11980f4b1e8cdf7577
Adham-Magdy <adhammagdy349@gmail.com>
2024-02-05 08:23:09 +0200

    style: implement style of delete issue button

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,10 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import { Box,Grid } from "@radix-ui/themes";
+import { Box,Flex,Grid } from "@radix-ui/themes";
 import delay from "delay";
 import { notFound } from "next/navigation";
 import React from "react";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
+import DeleteIssueButton from "./DeleteIssueButton";
 
 interface Props {
   params: { id: string }; // id in url path is default string
@@ -26,12 +27,15 @@ const IssueDetailsPage = async ({ params }: Props) => {
   }
   await delay(2000);
   return (
-    <Grid columns={{ initial: "1", md: "2" }} gap="5">
-      <Box>
+    <Grid columns={{ initial: "1", md: "5" }} gap="5">
+      <Box className="lg:col-span-4">
        <IssueDetails issue={issue}/>
       </Box>
       <Box>
+        <Flex direction={'column'} gap={'4'}>
         <EditIssueButton issueID={issue.id} />
+        <DeleteIssueButton issueID={issue.id}/>
+        </Flex>
       </Box>
     </Grid>
   );
